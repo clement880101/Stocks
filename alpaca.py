a8a03aaa5ad9bd142f9124f520de05ffc4b75534
clement880101 <clement880101@gmail.com>
2019-07-12 01:27:39 -0400

    Alpaca implementation update 1

--- alpaca.py
+++ alpaca.py
@@ -2,14 +2,39 @@
 
 import alpaca_trade_api as tradeapi
 
-id = 'PKC1DA6WJU5YXE2855E9'
-skey = 'JdaVsFcOGT/2zpPwebIkWxGtFKpJzJvY7NQTmYvf'
-
-api = tradeapi.REST(key_id=id,
-                    secret_key=skey,
+class Alpaca:
+    def __init__(self):
+        self.api = tradeapi.REST(key_id='PKC1DA6WJU5YXE2855E9',
+                    secret_key='JdaVsFcOGT/2zpPwebIkWxGtFKpJzJvY7NQTmYvf',
                     base_url='https://paper-api.alpaca.markets',
                     api_version='v2')
-account = api.get_account()
-if account.trading_blocked:
-    print('Account is currently restricted from trading.')
-print(account.buying_power)
+        self.account = self.api.get_account()
+        if self.account.trading_blocked:
+            print('Account is currently restricted from trading.')
+
+    def marketOrder(self, symbol, qty, side, tif):
+        self.api.submit_order(
+            symbol=symbol,
+            qty=qty,
+            side=side,
+            type='market',
+            time_in_force=tif,
+        )
+
+    def limitOrder(self, symbol, qty, side, tif, price):
+        self.api.submit_order(
+            symbol=symbol,
+            qty=qty,
+            side=side,
+            type='limit',
+            time_in_force=tif,
+            limit_price=price
+        )
+
+    def exsistingOrder(self):
+        orders = self.api.list_orders(
+            status='closed',
+            limit=100
+        )
+        print(orders)
+
